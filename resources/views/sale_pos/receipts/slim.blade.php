70f7004d18d0723678d29ca2300e02b5c2e34727
mwaurallan <allanmwaura2013@gmail.com>
2023-06-01 11:43:41 +0300

    changes

--- resources/views/sale_pos/receipts/slim.blade.php
+++ resources/views/sale_pos/receipts/slim.blade.php
@@ -38,13 +38,13 @@
 					<br/>
 				@endif
 
-				@if(!empty($receipt_details->address))
-					{!! $receipt_details->address !!}
-					<br/>
-				@endif
-				@if(!empty($receipt_details->contact))
-				<br/>{{ $receipt_details->contact }}
-				@endif
+{{--				@if(!empty($receipt_details->address))--}}
+{{--					{!! $receipt_details->address !!}--}}
+{{--					<br/>--}}
+{{--				@endif--}}
+{{--				@if(!empty($receipt_details->contact))--}}
+{{--				<br/>{{ $receipt_details->contact }}--}}
+{{--				@endif--}}
 			</p>
 				<br/>
 
